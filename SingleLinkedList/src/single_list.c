f580e3fa332e9f3d2b6c8caa9ac64890eff49d81
JBRS307 <kuba.rekas.307@gmail.com>
2024-10-05 00:21:25 +0200

    Done list initialization and basic push and peek operations

---
+++ SingleLinkedList/src/single_list.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "single_list.h"
+
+SingleLinkedList* single_list_init(size_t data_size) {
+    if (data_size == 0) {
+        return NULL;
+    }
+
+    SingleLinkedList* new = (SingleLinkedList*)malloc(sizeof(SingleLinkedList));
+    if (!new) {
+        return NULL;
+    }
+
+    new->head = NULL;
+    new->tail = NULL;
+    new->size = 0;
+    new->data_size = data_size;
+    return new;
+}
+
+SingleLinkedListStatus single_list_push(SingleLinkedList* list, const void* data) {
+    Node* new = (Node*)malloc(sizeof(Node));
+    if (!new) {
+        return MEMORY_ALLOCATION_ERROR;
+    }
+
+    new->data = malloc(list->data_size);
+    if (!new->data) {
+        free(new);
+        return MEMORY_ALLOCATION_ERROR;
+    }
+    new->next = NULL;
+    
+    memcpy(new->data, data, list->data_size);
+
+    if (!list->tail) {
+        list->head = list->tail = new;
+    } else {
+        list->tail->next = new;
+        list->tail = new;
+    }
+    list->size++;
+    return SUCCESS;
+}
+
+SingleLinkedListStatus single_list_at(const SingleLinkedList* list, size_t idx, void** data) {
+    if (idx >= list->size) {
+        return LIST_INDEX_OUT_OF_BOUNDS;
+    }
+    if (idx == list->size - 1) {
+        *data = list->tail->data;
+        return SUCCESS;
+    }
+
+    Node* curr = list->head;
+    size_t i = 0;
+    while (i < idx) {
+        curr = curr->next;
+        i++;
+    }
+
+    *data = curr->data;
+    return SUCCESS;
+}
---
+++ SingleLinkedList/src/single_list.h
@@ -0,0 +1,37 @@
+#ifndef __SINGLE_LIST
+#define __SINGLE_LIST
+
+#include <sys/types.h>
+
+typedef enum {
+    SUCCESS,
+    MEMORY_ALLOCATION_ERROR,
+    LIST_INDEX_OUT_OF_BOUNDS,
+} SingleLinkedListStatus;
+
+typedef struct Node Node;
+struct Node {
+    void* data;
+    Node* next;    
+};
+
+typedef struct {
+    Node* head;
+    Node* tail;
+    size_t size;
+    size_t data_size;
+} SingleLinkedList;
+
+// Dynamically initializes empty single linked list
+// with given data_size, returns NULL on error
+SingleLinkedList* single_list_init(size_t data_size);
+
+// Adds data to the end of the list. Copies at most data_size bytes
+// from memory pointed by data into the node at the end of the list
+SingleLinkedListStatus single_list_push(SingleLinkedList* list, const void* data);
+
+// Saves address pointed to by node at index idx into data
+// Allows modification of data
+SingleLinkedListStatus single_list_at(const SingleLinkedList* list, size_t idx, void** data);
+
+#endif
